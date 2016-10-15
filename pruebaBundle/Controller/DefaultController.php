7dfecbd3170fccaa4ba8116aef103bd20bc24326
Adria13 <adrichafes97@gmail.com>
2016-10-15 19:42:01 +0200

    Add files via upload

--- pruebaBundle/Controller/DefaultController.php
+++ pruebaBundle/Controller/DefaultController.php
@@ -4,20 +4,19 @@ namespace pruebaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+
+class DefaultController extends Controller{
+
+    public function indexAction(){
         return $this->render('pruebaBundle:Default:index.html.twig');
     }
 
-    public function dbAction()
-    {
+    public function dbAction($id) {
+    if($id == "db") {
         return $this->render('pruebaBundle:Default:db.html.twig');
     }
-
-    public function opAction()
-    {
-        return $this->render('pruebaBundle:Default:op.html.twig');
+    elseif($id == "op") {
+      return $this->render('pruebaBundle:Default:op.html.twig');
     }
+  }
 }
